e169dcee8fe5f47352700bbbbca06d2b6ebde3fa
Muhammad Shakir <90838750+prodevXpert@users.noreply.github.com>
2023-08-11 20:50:57 +0500

    Update Nav.jsx

--- src/layout/nav/Nav.jsx
+++ src/layout/nav/Nav.jsx
@@ -13,7 +13,7 @@ function classNames(...classes) {
 }
 export default function Nav() {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-gray-800 my-10 h-13">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
